c40765ef69e534441cba93c8b044ce8f826971af
Bill-Zuckuberg <thanh821329@gmail.com>
2021-11-29 23:23:58 +0700

    Added Button on bottom screen

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -2,6 +2,7 @@ import 'package:fl_exam_createdlayout/mics/colors.dart';
 import 'package:fl_exam_createdlayout/wedgits/app_buttons.dart';
 import 'package:fl_exam_createdlayout/wedgits/app_large_text.dart';
 import 'package:fl_exam_createdlayout/wedgits/app_text.dart';
+import 'package:fl_exam_createdlayout/wedgits/reponsive_buttom.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
@@ -22,6 +23,7 @@ class _DetailPageState extends State<DetailPage> {
         width: double.maxFinite,
         height: double.maxFinite,
         child: Stack(
+          // fit: StackFit.expand,
           children: [
             Positioned(
                 child: Container(
@@ -33,127 +35,187 @@ class _DetailPageState extends State<DetailPage> {
                       fit: BoxFit.cover)),
             )),
             Positioned(
-                top: 280,
                 child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20))),
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                AppLargetText(
-                                  text: 'Xa lộ',
-                                  color: Colors.black,
-                                ),
-                              ],
-                            ),
-                            AppLargetText(
-                              text: '\$ 250',
-                              color: AppColors.mainColor,
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.location_on,
-                              color: AppColors.mainColor,
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            AppText(
-                              text: 'Đơn đọc một chặng đường',
-                              color: AppColors.mainColor,
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Wrap(
-                              children: List.generate(5, (index) {
-                                return Icon(
-                                  Icons.star,
-                                  color: index < gettenStar
-                                      ? AppColors.starColor
-                                      : AppColors.textColor2,
-                                );
-                              }),
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            AppText(
-                              text: '(4.0)',
-                              color: AppColors.textColor2,
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        AppLargetText(
-                          text: "People",
-                          size: 16,
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        AppText(
-                          text: 'Number of people in your group',
-                          color: AppColors.textColor2,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Wrap(
-                          children: List.generate(
-                              5,
-                              (index) => InkWell(
-                                    onTap: () => setState(() {
-                                      selectIndex = index;
-                                      print(index);
-                                    }),
-                                    child: Container(
-                                      margin: const EdgeInsets.only(
-                                          top: 10, right: 10),
-                                      child: AppButton(
-                                        text: (index + 1).toString(),
-                                        size: 50,
-                                        color: selectIndex == index
-                                            ? Colors.white
-                                            : Colors.black,
-                                        backgroundColor: selectIndex == index
-                                            ? Colors.black
-                                            : AppColors.buttonBackground,
-                                        borberColor: selectIndex == index
-                                            ? Colors.black
-                                            : AppColors.buttonBackground,
-                                      ),
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.flare_sharp,
+                        color: Colors.white,
+                      )),
+                ],
+              ),
+            )),
+            Positioned(
+              top: 280,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppLargetText(
+                                text: 'Xa lộ',
+                                color: Colors.black,
+                              ),
+                            ],
+                          ),
+                          AppLargetText(
+                            text: '\$ 250',
+                            color: AppColors.mainColor,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: AppColors.mainColor,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          AppText(
+                            text: 'Đơn đọc một chặng đường',
+                            color: AppColors.mainColor,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: List.generate(5, (index) {
+                              return Icon(
+                                Icons.star,
+                                color: index < gettenStar
+                                    ? AppColors.starColor
+                                    : AppColors.textColor2,
+                              );
+                            }),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          AppText(
+                            text: '(4.0)',
+                            color: AppColors.textColor2,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      AppLargetText(
+                        text: "People",
+                        size: 16,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      AppText(
+                        text: 'Number of people in your group',
+                        color: AppColors.textColor2,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Wrap(
+                        //Bug can't setState
+                        children: List.generate(
+                            5,
+                            (index) => InkWell(
+                                  onTap: () => setState(() {
+                                    selectIndex = index;
+                                  }),
+                                  child: Container(
+                                    margin: const EdgeInsets.only(
+                                        top: 10, right: 10),
+                                    child: AppButton(
+                                      text: (index + 1).toString(),
+                                      size: 50,
+                                      color: selectIndex == index
+                                          ? Colors.white
+                                          : Colors.black,
+                                      backgroundColor: selectIndex == index
+                                          ? Colors.black
+                                          : AppColors.buttonBackground,
+                                      borberColor: selectIndex == index
+                                          ? Colors.black
+                                          : AppColors.buttonBackground,
                                     ),
-                                  )),
-                        )
-                      ],
-                    ),
+                                  ),
+                                )),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      AppLargetText(
+                        text: 'Description',
+                        color: Colors.black87,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      AppText(
+                        text:
+                            'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+                        color: AppColors.textColor2,
+                      ),
+                    ],
                   ),
+                ),
+              ),
+            ),
+            Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Row(
+                  children: [
+                    AppButton(
+                      size: 45,
+                      isIcon: true,
+                      icon: Icons.favorite_border,
+                      borberColor: AppColors.textColor1,
+                      backgroundColor: Colors.white,
+                      color: AppColors.textColor1,
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    ReponsiveButtom(
+                      isReponsive: true,
+                      text: 'Book to Trip now',
+                    )
+                  ],
                 ))
           ],
         ),
--- lib/wedgits/reponsive_buttom.dart
+++ lib/wedgits/reponsive_buttom.dart
@@ -1,25 +1,45 @@
 import 'package:fl_exam_createdlayout/mics/colors.dart';
+import 'package:fl_exam_createdlayout/wedgits/app_text.dart';
 import 'package:flutter/material.dart';
 
 class ReponsiveButtom extends StatelessWidget {
   ReponsiveButtom(
-      {Key? key, this.isReponsive = false, this.height = 45, this.width})
+      {Key? key,
+      this.isReponsive = false,
+      this.height = 45,
+      this.width,
+      this.text = ''})
       : super(key: key);
 
   bool? isReponsive;
   double? width;
   double? height;
+  String text;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10), color: AppColors.mainColor),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Image.asset('assets/arrowright.png')],
+    return Flexible(
+      child: Container(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        height: height,
+        width: isReponsive == true ? double.maxFinite : width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: AppColors.mainColor),
+        child: Row(
+          mainAxisAlignment: isReponsive == true
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.center,
+          children: [
+            isReponsive == true
+                ? AppText(
+                    text: text,
+                    color: Colors.white,
+                  )
+                : Container(),
+            Image.asset('assets/arrowright.png')
+          ],
+        ),
       ),
     );
   }
